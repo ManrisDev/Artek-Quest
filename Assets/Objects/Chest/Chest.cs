55ed4d273b69911e3f603a0500f2cfcbbc957162
Manris <polesko02@mail.ru>
2025-02-28 19:43:38 +0300

    Load

---
+++ Assets/Objects/Chest/Chest.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Varwin.Public;
+
+namespace Varwin.Types.Chest_af11eb3363a5470791085694463ffb25
+{
+    [VarwinComponent(English: "Chest")]
+    public class Chest : VarwinObject
+    {
+        private bool _isOpened = false;
+        private bool IsOpened
+        {
+            get => _isOpened; 
+            set
+            {
+                _isOpened = value;
+                _animator.SetBool("IsOpen", value);
+            }
+        }
+
+        private Animator _animator;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        [Checker(English: "Chest is opened", Russian: "Сундук открыт")]
+        public bool ChestIsOpened() => IsOpened;
+
+        [ActionGroup("Top")]
+        [Action(English: "Open the chest", Russian: "Открыть сундук")]
+        public void OpenTheChest() => IsOpened = true;
+        [ActionGroup("Top")]
+        [Action(English: "Close the chest", Russian: "Закрыть сундук")]
+        public void CloseTheChest() => IsOpened = false;
+    }
+}
