f593ce798804560cb1b87702ceaf0791538e7629
Manris <polesko02@mail.ru>
2025-03-02 03:08:45 +0300

    Done

--- Assets/Objects/Chest/Chest.cs
+++ Assets/Objects/Chest/Chest.cs
@@ -9,7 +9,7 @@ namespace Varwin.Types.Chest_af11eb3363a5470791085694463ffb25
         private bool _isOpened = false;
         private bool IsOpened
         {
-            get => _isOpened; 
+            get => _isOpened;
             set
             {
                 _isOpened = value;
@@ -17,18 +17,30 @@ namespace Varwin.Types.Chest_af11eb3363a5470791085694463ffb25
             }
         }
 
+        [Header("Sounds")]
+        public AudioClip OpenSound;
+        public AudioClip CloseSound;
+
         [VarwinInspector(English: "Close chest on start")]
         public bool CloseOnStart { get; set; }
 
+        [Tooltip("Float from 0 to 1.")]
+        [VarwinInspector(English: "Sounds volume")]
+        public float SoundsVolume { get; set; } = 1f;
+
+        private AudioSource _audioSource;
         private Animator _animator;
 
         private void Awake()
         {
+            _audioSource = GetComponent<AudioSource>();
             _animator = GetComponent<Animator>();
         }
         private void Start()
         {
             IsOpened = !CloseOnStart;
+            if (SoundsVolume >= 0 || SoundsVolume <= 1)
+                _audioSource.volume = SoundsVolume;
         }
 
         [Checker(English: "Chest is opened", Russian: "Сундук открыт")]
@@ -36,9 +48,17 @@ namespace Varwin.Types.Chest_af11eb3363a5470791085694463ffb25
 
         [ActionGroup("Top")]
         [Action(English: "Open the chest", Russian: "Открыть сундук")]
-        public void OpenTheChest() => IsOpened = true;
+        public void OpenTheChest()
+        {
+            _audioSource.PlayOneShot(OpenSound);
+            IsOpened = true;
+        }
         [ActionGroup("Top")]
         [Action(English: "Close the chest", Russian: "Закрыть сундук")]
-        public void CloseTheChest() => IsOpened = false;
+        public void CloseTheChest()
+        {
+            _audioSource.PlayOneShot(CloseSound);
+            IsOpened = false;
+        }
     }
 }
